dec84daba68bb8aae090880b6da555df2e8e37d5
hawkeye <173827852+hawkeye1900@users.noreply.github.com>
2024-10-05 07:07:55 +0100

    first commit. App, Header, CurrentDogs, Footer components created. props for first CurrentDogs set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const currentDogs = {
+  name: "Bear",
+  sex: "Male",
+  age: 3,
+  photo: "/images/bear.jpg",
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <CurrentDogs />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Save Our Shepherds - German Shepherd Rescue</h1>;
+}
+
+function CurrentDogs() {
+  return (
+    <div>
+      <h2>Shepherds looking for a home</h2>
+      <Dog name="Bear" sex="Male" age="3" photo="/images/bear.jpg" />
+      <Dog />
+      <Dog />
+    </div>
+  );
+}
+
+function Footer() {
+  const openHours = 12;
+  const closedHours = 16;
+  const hour = new Date().getHours();
+  const isOpen = hour >= openHours && hour <= closedHours;
+
+  return (
+    <footer>
+      <p>Save Our Shepherds</p>
+      <p>
+        We are currently {isOpen ? "open" : "closed"} for phone enquiries.
+        {isOpen ? "" : " Please use our contact form."}
+      </p>
+      <p>We open for phone enquiries from 12:00 - 16:00hrs local time.</p>
+      <p>
+        {new Date().toLocaleTimeString()} 0n {new Date().toLocaleDateString()}
+      </p>
+    </footer>
+  );
+}
+
+function Dog(props) {
+  return (
+    <div>
+      <img src={props.photo} alt="GSD called {props.name}" />
+      <h2>Name: {props.name}</h2>
+      <p>Sex: {props.sex}</p>
+      <p>Age: {props.age}</p>
+      <button>Find out more</button>
     </div>
   );
 }
