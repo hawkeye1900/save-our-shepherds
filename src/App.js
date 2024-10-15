9e8a9a257536bbc67589db65edd9e09007678dcf
hawkeye <173827852+hawkeye1900@users.noreply.github.com>
2024-10-15 14:06:38 +0100

    currentDogs populated with data on other dogs, the used map() to iterate over currentDogs list and passed each dog object to <Dog /> component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import "./App.css";
 
-const currentDogs = {
-  name: "Bear",
-  sex: "Male",
-  age: 3,
-  photo: "/images/bear.jpg",
-};
+const currentDogs = [
+  {
+    name: "Bear",
+    sex: "Male",
+    age: 3,
+    photo: "/images/bear.jpg",
+  }, 
+  {
+    name: "Rose",
+    sex: "Female",
+    age: 2,
+    photo: "/images/rose.jpg",
+  }, 
+  {
+    name: "Vader",
+    sex: "Male",
+    age: 3,
+    photo: "/images/vader.jpg",
+  },
+  {
+    name: "Prince",
+    sex: "Male",
+    age: 3,
+    photo: "/images/prince.jpg",
+  }, 
+   {
+    name: "Goofy",
+    sex: "Male",
+    age: 5,
+    photo: "/images/goofy.jpg",
+  },
+   {
+    name: "Sofia",
+    sex: "Female",
+    age: 4,
+    photo: "/images/sofia.jpg",
+  }
+];
 
 function App() {
   return (
@@ -21,13 +53,26 @@ function Header() {
   return <h1>Save Our Shepherds - German Shepherd Rescue</h1>;
 }
 
+function Dog(props) {
+  return (
+    <div>
+      <img src={props.photo} alt="GSD called {props.name}" />
+      <h2>Name: {props.name}</h2>
+      <p>Sex: {props.sex}</p>
+      <p>Age: {props.age}</p>
+      <button>Find out more</button>
+    </div>
+  );
+}
+
 function CurrentDogs() {
   return (
     <div>
       <h2>Shepherds looking for a home</h2>
-      <Dog name="Bear" sex="Male" age="3" photo="/images/bear.jpg" />
-      <Dog />
-      <Dog />
+      <div>
+        {currentDogs.map(dog => <Dog name={dog.name} sex={dog.sex} age={dog.age} photo={dog.photo}/>)}
+      </div>
+      
     </div>
   );
 }
@@ -45,23 +90,11 @@ function Footer() {
         We are currently {isOpen ? "open" : "closed"} for phone enquiries.
         {isOpen ? "" : " Please use our contact form."}
       </p>
-      <p>We open for phone enquiries from 12:00 - 16:00hrs local time.</p>
+      <p>We open for phone enquiries from 12:00 - 16:00hrs our time.</p>
       <p>
         {new Date().toLocaleTimeString()} 0n {new Date().toLocaleDateString()}
       </p>
     </footer>
-  );
-}
-
-function Dog(props) {
-  return (
-    <div>
-      <img src={props.photo} alt="GSD called {props.name}" />
-      <h2>Name: {props.name}</h2>
-      <p>Sex: {props.sex}</p>
-      <p>Age: {props.age}</p>
-      <button>Find out more</button>
-    </div>
   );
 }
 
